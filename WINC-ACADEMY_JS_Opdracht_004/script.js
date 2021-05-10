43aed617ae246ac6dad572653e070feff3239959
malutonnaer <81236935+malutonnaer@users.noreply.github.com>
2021-05-10 11:19:01 +0200

    Add files via upload

---
+++ WINC-ACADEMY_JS_Opdracht_004/script.js
@@ -0,0 +1,39 @@
+var myAge = 30;
+var myName = "Job";
+var myName = "Malu";
+var totalAmount = 50;
+
+
+if (myAge >= 18 && myAge <= 25) {
+    console.log("Je bent tussen de 18 en de 25 jaar en krijgt 50% korting");
+}
+
+else {
+    console.log("Jouw leeftijd komt niet in aanmerking voor de korting");
+}
+
+if (myName === "Bram" || myName === "Sarah" ) {
+    console.log("Yes, jij heet Sarah of Bram en krijgt een gratis biertje");
+}
+
+else {
+    console.log("Jij mag een ook biertje maar betaalt hier €1,50 voor");
+}
+
+if (totalAmount >= 100) {
+    console.log("Je bestelt voor minimaal 100 euro aan drank en krijgt daarom een flesje Champagne van het huis!");
+} else if (totalAmount >= 50) {
+    console.log("Je bestelt voor minimaal 50 euro aan drank en krijgt daarom een portie Nachos van het huis!");
+} else if (totalAmount >= 25) {
+    console.log("Je bestelt voor minimaal 25 euro aan drank en krijgt daarom een portie bitterballen van het huis!");
+} else {
+    console.log("Jouw bestelling is te laag om een presentje van het huis te ontvangen");
+}
+ 
+
+
+
+
+
+
+ 
